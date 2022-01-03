40fb06b11d879e2ebb2ddc89b7b636a6cc0e3524
dayo <dayo05@daum.net>
2022-01-03 12:39:44 +0900

    Initial commit

---
+++ DiTalks-D2K/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using Discord;
+using Discord.WebSocket;
+
+var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+socket.Bind(new IPEndPoint(IPAddress.Any, 8080));
+socket.Listen(10);
+
+var clientSocket = socket.Accept();
+
+var client = new DiscordSocketClient();
+client.Log += e =>
+{
+    Console.WriteLine(e);
+    return Task.CompletedTask;
+};
+client.Ready += () =>
+{
+    Console.WriteLine($"{client.CurrentUser} connected");
+    return Task.CompletedTask;
+};
+client.MessageReceived += async message =>
+{
+    clientSocket.Send(Encoding.UTF8.GetBytes(message.Content));
+};
+
+await client.LoginAsync(TokenType.Bot, "NzQwODQ5NzIxMjQwOTc3NDM3.XyvAEQ.qT52IluqnMpK5p3fw2WwLeipLO4");
+await client.StartAsync();
+await Task.Delay(Timeout.Infinite);
---
+++ DiTalks-test/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+socket.Connect(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8080));
+while (true)
+{
+    var buf = new byte[100000];
+    socket.Receive(buf);
+    Console.WriteLine(Encoding.UTF8.GetString(buf));
+}
